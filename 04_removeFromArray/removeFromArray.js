ff8640ef934085108523ff86c8b9dc150fec0cea
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2023-06-04 19:48:37 +0200

    Complete exc 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
 
+    iterateArray: for (let i = 0; i < array.length; i++) {
+        for (const arg of args) {
+            if (arg === array[i]) continue iterateArray;
+        }
+        newArray.push(array[i]);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
