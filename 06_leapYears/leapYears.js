fce1212c2ebab6dd61fa8c192b7eb900741fc492
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2023-06-04 23:17:55 +0200

    Complete exc 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // if (!(year % 4)) {
+    //     if (!(year % 400) && !(year % 100)) {
+    //         return true
+    //     } else if (!(year % 100)) {
+    //         return false
+    //     } else {
+    //         return true;
+    //     }
+    // } else {
+    //     return false
+    // }
 
+    if (!(year % 4))
+        if (!(year % 400) && !(year % 100)) return true;
+        else if (!(year % 100)) return false;
+        else return true;
+    else
+        return false
 };
 
 // Do not edit below this line
