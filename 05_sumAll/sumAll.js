2aa7ab1405edfb8359e416c12abd686c33e1acae
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2023-06-04 23:03:20 +0200

    Complete exc 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
+    
+    let sum = 0;
 
+    let lowerNum = (num1 < num2) ? num1 : num2;
+    let higherNum = (num2 > num1) ? num2 : num1;
+
+    for (let i = lowerNum; i <= higherNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
