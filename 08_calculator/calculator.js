767949fd011991d7efc5bca962330e7b8b5ac300
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2023-06-11 00:54:58 +0200

    Write power function for exc 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,8 +14,8 @@ const multiply = function(arr) {
   return arr.reduce((sum, num) => sum * num);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
 const factorial = function() {
