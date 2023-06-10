cb886bfcca1dce4b11828d6ddaab9f3d9cff4803
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2023-06-11 01:28:26 +0200

    Finish exc 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let onlyLowerChars = string.toLowerCase().replaceAll(/[^a-z]|_/g, '');
+    let revOnlyLowerChars = onlyLowerChars.split('').reverse().join('')
+    return onlyLowerChars == revOnlyLowerChars;
 };
 
 // Do not edit below this line
