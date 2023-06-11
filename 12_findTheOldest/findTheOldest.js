a241f2ef74991570615c16387cd086507a1abb5b
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2023-06-11 02:07:16 +0200

    Finish exc 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    arr.sort((a, b) => {
+        if (!b.yearOfDeath) b.yearOfDeath = new Date().getFullYear()
+        if (!a.yearOfDeath) a.yearOfDeath = new Date().getFullYear()
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
+    });
+    return arr[0]
 };
 
 // Do not edit below this line
